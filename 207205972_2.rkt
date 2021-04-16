c379b3ff5ed255d0c42e427b4b7617445b3f2b2b
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2021-04-16 17:36:49 +0300

    init

---
+++ 207205972_2.rkt
@@ -0,0 +1,21 @@
+#lang pl 02
+
+
+#| BNF for the SE language:
+ <SE> ::= { <FinalNUM> }
+ | { + <SE> <SE> }
+ | { - <AE> <SE> }
+ | { * <SE> <SE> }
+ | { / <SE> <SE>  }
+
+
+<FinalNUM> ::=  <NUMd> | <NUMc> | <NUMs>
+
+<Digit> ::= 0|1|2|3|4|5|6|7|8|9|
+<NUMd> ::= <digit> | <NUM><digit>
+
+<CharNum> ::= #\0| #\1| #\2| #\3| #\4| #\5| #\6| #\7| #\8| #\9
+<NUMc> ::= <CharNum> | <NUMc><CharNum>
+
+<NUMs> ::= (string <NUMc>)
+|#
