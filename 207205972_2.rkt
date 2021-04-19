afb192ce5ddc5f07858228992298fab9cb8861b8
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2021-04-19 12:40:24 +0300

    Update 207205972_2.rkt

--- 207205972_2.rkt
+++ 207205972_2.rkt
@@ -5,9 +5,7 @@
 #|
 Exercise: Ex 2
 By: Yarden Cohen 207205972
-
  Q1.1
-
  BNF for the SE language:
  <SE> ::=  <NUM> 
  | { str-app   <SE>  }
@@ -15,17 +13,12 @@ By: Yarden Cohen 207205972
  | { num2str   <SE>  }
  
 <NUM> ::=  <NUMd> | <NUMc> | <NUMs> | <SE><NUMs> | <SE><NUMc> | <SE><NUMd> | <NUM><SE> | <SE><NUM> | <SE>
-
 <DIGITd> ::= 0|1|2|3|4|5|6|7|8|9
 <NUMd> ::= <DIGITd> | <NUM><DIGITd>
-
 <DIGITc> ::= #\0| #\1| #\2| #\3| #\4| #\5| #\6| #\7| #\8| #\9
 <NUMc> ::= <DIGITc> | <NUMc><DIGITc>
-
 <NUMs> ::= (string <NUMc>) | "<NUMd>"
-
   Q1.2
-
 ( string-append "45" ( number->string ( string-length "0033344" )) ( string #\1 #\2 #\4 ))
 <SE> => { str-app   <SE>  }
      => { str-app   <NUM> }
@@ -39,7 +32,6 @@ By: Yarden Cohen 207205972
      => { str-app    <NUMs> { num2str   7  }( string #\1 #\2 #\4 ) }
      => { str-app    "<NUMd>" { num2str   7  }( string #\1 #\2 #\4 ) }
      => { str-app    "45" { num2str   7  }( string #\1 #\2 #\4 ) }
-
 ( string-append "333" ( string-insert "1357" #\4 66 )  ( string #\5 #\7 #\9 )) 
 <SE> => { str-app   <SE>  }
      => { str-app   <NUM> }
@@ -74,4 +66,50 @@ By: Yarden Cohen 207205972
      => { str-app   { num2str   <SE>  } { str-insrt "1357" #\4 66  } }
      => { str-app   { num2str   <NUMd>  } { str-insrt "1357" #\4 66  } }
      => { str-app   { num2str   156879  } { str-insrt "1357" #\4 66  } }
-|# 
+|#
+
+
+;;--------------------------------------------Q2--------------------------------------------------------
+
+#|
+input: some number
+output: calc (num)^2
+This is an auxiliary function that calculates a number squared |#
+(: square : Number -> Number)
+(define(square num)
+   (* num num))  
+
+#|
+input: list of numbers
+output: number which is the sum of the squares of all of the numbers in the list
+This function uses `foldl` when-
+`proc` is : '+'
+`init` starts from 0
+`lst` is a list that each element in it used the square function when square uses a map.
+map runs square function on each of the list elements.
+This question was not difficult for me and took me an average of 30 minutes.
+The difficulty was mainly the understanding how map and foldl work and how to combine them together.|#
+(: sum-of-squares : (Listof Number) -> Number)
+(define(sum-of-squares lst)
+  (let ([afterSquare (map square lst) ])
+    (foldl + 0 afterSquare)))
+
+
+(test (square 5) => 25)
+(test (square -4) => 16)
+(test (square 4) => 16)
+(test (square 0) => 0)
+
+(test (sum-of-squares '()) => 0)
+(test (sum-of-squares '(5)) => 25)
+(test (sum-of-squares '(2 6)) => 40)
+(test (sum-of-squares '(1 2 3)) => 14)
+(test (sum-of-squares '(-1 2 -3)) => 14)
+(test (sum-of-squares '(-1 -2 -3)) => 14)
+(test (sum-of-squares '(0 1 2 3 0)) => 14)
+(test (sum-of-squares '(2 2 2 2 2 2)) => 24)
+
+
+
+
+
